560cf3118e216dc18755c45a82991c90497231a6
Mohammad Wasim <wasim.html@gmail.com>
2021-10-25 08:43:09 +0600

    initial commit

---
+++ radioplayer.php
@@ -0,0 +1,20 @@
+<div align="center" style="background:#f00">
+<script type="text/javascript" src="player.js"></script>
+<script type="text/javascript">
+MRP.insert({
+'url':'http://167.114.131.90:5534/;',
+'codec':'mp3',
+'volume':100,
+'autoplay':true,
+'forceHTML5':true,
+'jsevents':true,
+'buffering':0,
+'title':'Ambient',
+'welcome':'WELCOME TO Radio Dhoom 24',
+'skin':'substream',
+'width':260,
+'height':50,
+});
+</script>
+
+</div>
